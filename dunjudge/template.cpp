ff4de414f10994faa23f71a914d33060bb426206
SoInstant <44108612+SoInstant@users.noreply.github.com>
2020-11-07 17:35:07 +0800

    Update template.cpp

--- dunjudge/template.cpp
+++ dunjudge/template.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long // ~long long man~
+#define int long long // ~~~LONG LONG MAN~~~
 
-//define variables here
+// define variables here
 
 int32_t main() {
 	ios::sync_with_stdio(0);
-	cin.tie(0);
-	// code here
-	return 0;
+  	cin.tie(0);
+  	cout.tie(0);
+	// insert code here
+  	return 0;
 }
