890ee13f274c111ccd8fa84a104d6ed2dd6ae7dd
SoInstant <44108612+SoInstant@users.noreply.github.com>
2020-11-21 23:06:11 +0800

    Update problem A.cpp
    fail....

--- codeforces/div 2 - round 685/problem A.cpp
+++ codeforces/div 2 - round 685/problem A.cpp
@@ -6,12 +6,40 @@ using namespace std;
 #define inf_int 1e9
 #define NEWLINE cout << "\n"
 
-// define variables here
+int t;
+
+int smallest_factor(int x) {
+	for (int i = 2; i <= sqrt(x); ++i) {
+		if (x%i==0) return i;
+	}
+	return 1;
+}
 
 int32_t main() {
 	ios::sync_with_stdio(0);
   	cin.tie(0);
   	cout.tie(0);
-	//code here
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		int n; cin >> n;
+		int counter = 0;
+		while (n != 1) {
+			if (n == 2){
+				n--;
+				counter++;
+				continue;
+			} else if (n == 3) {
+				n--;
+				counter++;
+				continue;
+			}
+			int rmd = smallest_factor(n);
+			if (rmd > n - 1) n--;
+			else n = rmd;
+			counter++;
+		}
+		cout << counter;
+		NEWLINE;
+	}
   	return 0;
 }
